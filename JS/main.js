3d1688d6c16094d6b114b8b48f8e02c757fe20c6
Nalbert <nalbertsampaio@gmail.com>
2021-07-05 18:12:21 -0300

    meu primeiro javascript

---
+++ JS/main.js
@@ -0,0 +1,95 @@
+
+alert("Isso vai ser legen...");
+alert("espera um pouquinho...");
+//alert("mais uma pausa dramática...");
+alert("dário,leegeendáriooo!!!!!!!");
+
+//function isso(){
+ // alert("haahaaa");
+//}
+
+function clicou(){
+ window.open ("https://www.primevideo.com/detail/0HX4L62NMBMKP5QXU8QNR8958G/ref=atv_dp_season_select_s1?language=pt_BR");
+}
+
+function trocar(){
+  document.getElementById("mouse").innerHTML = "SOU INCRÍVEL";
+}
+
+function voltar(){
+  document.getElementById("mouse").innerHTML="Desenvolvido por NHMS";
+} 
+
+//function clicou(){
+  // document.getElementById("legen").innerHTML = "Isso vai ser legen...espera um pouquinho...dário,legendário";
+//}
+//function clicou(){
+  //window.open ("https://www.primevideo.com/detail/0HX4L62NMBMKP5QXU8QNR8958G/ref=atv_dp_season_select_s1?language=pt_BR");
+//}
+
+//function clicou(){
+ // document.getElementById("escita").innerHTML = "Isso vai ser legen...espera um pouquinho...dário,legendário";
+  //alert("Obrigado por clicar");
+  //}
+//function redirecionar(){
+  //window.open(" link ");
+  //window.location.href=" link ";
+//}
+
+//function validaIdade(idade){
+    //var validar;
+    //if (idade >= 18){
+      //  validar =true
+    //}else{
+  //      validar = false
+  //  }return validar
+//}
+//var idade = prompt(" Qual sua idade?");
+//console.log(validaIdade(idade));
+
+//var d = new Date();
+//alert(d);
+//alert(d.getDay());
+//alert(d.getHours());
+//alert(d.getMinutes());
+
+//var count;
+//for(count=0;count<=5;count++){
+  //  alert(count);
+//}
+
+//var count = 0;
+//while (count <= 5){
+  //  console.log(count);
+   // count ++ ;
+//};
+
+//var idade = prompt("qual sua idade?");
+//if (idade >= 18){
+  //  alert("maior de idade");
+//}else{
+  //  alert("menor de idade");
+//}
+
+
+//var frutas = [{nome:"maça",cor:"vermelha"},{nome:"uva" , cor:"roxa"}]
+//console.log(frutas);
+//alert(frutas[1].nome);
+
+//var fruta = {nome:"maça",cor:"vermelha"}
+//console.log(fruta.nome);
+//alert(fruta.cor);
+
+//var lista =["maça", "pera","laranja"];
+//lista.push("uva");
+//lista.pop();
+
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" - "))
+
+//var nome = " Nalbert Sampaio ";
+//var frase = "você é incrível";
+//alert("Bem vindo" + nome);
+//alert( frase);
+//console.log(nome + frase);
